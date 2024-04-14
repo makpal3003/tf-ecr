828c0659c588b5f6ee3d59de5a6c9a52110b2e3b
makpal3003 <makpal.aitkazinova@gmail.com>
2024-04-13 23:27:06 -0500

    new file added

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-ecr-makpal"
+    key            = "tf-ecr"
+    region         = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name                 = "my-ecr-repo"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
